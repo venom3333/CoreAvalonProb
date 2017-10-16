fd35abe94aae2951e292959d9a64fc19aebede0d
vovan <venom3333@gmail.com>
2017-10-16 20:48:45 +0300

    Init Commit

---
+++ CoreProb/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Diagnostics;
+using Avalonia.Logging.Serilog;
+using Avalonia.Themes.Default;
+using Avalonia.Markup.Xaml;
+using Serilog;
+
+namespace CoreProb
+{
+    class App : Application
+    {
+
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+            base.Initialize();
+        }
+
+        static void Main(string[] args)
+        {
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+    }
+}
